e203cd045486f47cbaeb2e0ac547833c0e76a5d0
vp-msdos <vp.msdos@gmail.com>
2017-08-06 20:24:30 +0530

    Adding sha256 and salt function

--- php/login.php
+++ php/login.php
@@ -1,4 +1,6 @@
 <?php
 $data = json_decode(file_get_contents("php://input"));
-echo $data->name;
+$salt = bin2hex(mcrypt_create_iv(32,MCRYPT_DEV_URANDOM));
+$hashedPwd = hash('256',$salt.$data.password);
+echo $hashedPwd;
 ?>
