a6006316d64945d7d733ec66496e11d39f6871b8
vp-msdos <vp.msdos@gmail.com>
2017-08-06 14:05:18 +0530

    adding sign up functionality

---
+++ scripts/js/own/signup.js
@@ -0,0 +1,8 @@
+'use strict'
+var app = angular.module("signup",[]);
+app.controller("signupController",function($scope){
+$scope.adminList = ['vp.msdos@gmail.com','prad33p.verma@gmail.com','nripendra.bajpai@gmail.com'];  
+$scope.save = function(){
+
+};
+});
