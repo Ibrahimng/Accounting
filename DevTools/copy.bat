f72686a744d38bfd6ceb2ee4f1b3f78a8b0478b3
vp-msdos <vp.msdos@gmail.com>
2017-08-06 13:11:43 +0530

    Adding bat file to deploy code on wamp www folder

---
+++ DevTools/copy.bat
@@ -0,0 +1,3 @@
+rem First path is the code path where you are developing code
+rem Second path is wamp server's www folder path where will be deplying the code after double clicking on this batch it will copy content
+ROBOCOPY "E:\NodejsWork\Accounting" "C:\wamp\www" /mir
--- scripts/js/own/login.js
+++ scripts/js/own/login.js
@@ -5,11 +5,11 @@ login.controller("loginController",function($scope, $window, $http){
 $scope.submit = function(){
 
 if($scope.userName === "vp.msdos@gmail.com" && $scope.pwd === "test"){
-   $window.location.href="main.html";
+   //$window.location.href="main.html";
    //Once deployed on server use ajax as mentioned below.
-   /*$http.get("main.html").then(function(response){
+   $http.get("main.html").then(function(response){
   $scope.res = response.data;
-});*/
+});
 }
 };
 });
