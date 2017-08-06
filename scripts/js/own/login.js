5d9ce38b0211823b97840d9782d39fbe52dcff18
vp-msdos <vp.msdos@gmail.com>
2017-08-06 19:46:18 +0530

    Adding angular js post call to php for login flow

--- scripts/js/own/login.js
+++ scripts/js/own/login.js
@@ -1,15 +1,42 @@
 'use strict'
 
 var login = angular.module("login",[]);
-login.controller("loginController",function($scope, $window, $http){
+login.controller("loginController",function($scope, $window, $http,$templateCache){
+$scope.codeStatus="";
 $scope.submit = function(){
+  var method = 'POST';
+  var url = 'php/login.php';
+  var FormData = {
+    'name' : $scope.userName,
+    'password' : $scope.pwd
+  };
 
-if($scope.userName === "vp.msdos@gmail.com" && $scope.pwd === "test"){
+  $http({
+    method: method,
+    url: url,
+    data: FormData,
+    headers: {'Content-Type': 'application/json'},
+    cache: $templateCache
+  }).
+  then(function(response) {
+
+   $scope.codeStatus = response.status;
+   if(response.status === 200){
+     $window.location.href="main.html";
+}else{
+  console.log("no redirect");
+}
+
+  },function(err){
+
+  });
+  return false;
+};
+
+/*if($scope.userName === "vp.msdos@gmail.com" && $scope.pwd === "test"){
    //$window.location.href="main.html";
    //Once deployed on server use ajax as mentioned below.
    $http.get("main.html").then(function(response){
   $scope.res = response.data;
-});
-}
-};
+});*/
 });
