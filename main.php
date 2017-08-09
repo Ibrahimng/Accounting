78fc44039d1ecacfedb5d19daa4f5d36a5fa4eef
Sanorous <30567881+Sanorous@users.noreply.github.com>
2017-08-09 22:11:29 +0530

    Storing data into database
    made some changes and used password_hash in php

--- main.php
+++ main.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Test</title>
+    <title>Success</title>
   </head>
   <body>
-
+<h1 style="color:brown">Success</h1>
   </body>
 </html>
--- php/signup.php
+++ php/signup.php
@@ -1,23 +1,33 @@
 <?php ob_start(); ?>
 <?php
+$server="localhost";
+$username="root";
+$password="";
+$database="billing";
+$conn = new mysqli($server, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("connection failed:" . $conn->connect_error);
+}
 
 
  //This gets all the other information from the form
  $Email=$_POST['Email'] ;
  $Password= $_POST['Password'] ;
- $Salt= $_POST['Salt'] ;
+ $PasswordConfirm= $_POST['PasswordConfirm'] ;
+ $Hashed= password_hash('$Password', PASSWORD_DEFAULT, ['cost' => 10]);
+ //$Salt= $_POST['Salt'] ;
  $Mobile= $_POST['Mobile'] ;
  $Updated_by= $_POST['Updated_by'] ;
 
-
  // Connects to your Database
- mysql_connect("localhost", "root", "1234") or die(mysql_error()) ;
- mysql_select_db("billing") or die(mysql_error()) ;
+ //$conn = new mysqli("localhost", "root", "", "billing") ;
+ //mysql_select_db("billing") or die(mysql_error()) ;
 
  //Writes the information to the database
- mysql_query("INSERT INTO `login`(Email,Password,Salt,Mobile,Updated_by)
-		 VALUES ('$Email','$Password','$Salt','$Mobile','$Updated_by')") ;
+ $sql="INSERT INTO `login`(Email,Password,PasswordConfirm,Hashed,Mobile,Updated_date,Updated_by)
+		 VALUES ('$Email','$Password','$PasswordConfirm','$Hashed','$Mobile',now(),'$Updated_by')";
 
- header("Location: main.php");
+ header("Location: ../main.php");
  ?>
 <?php ob_end_flush(); ?>
