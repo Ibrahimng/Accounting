b63c25ad1684afd910b2da26502f279a41e17ea7
Sanorous <30567881+Sanorous@users.noreply.github.com>
2017-08-10 10:33:21 +0530

    signup.php changes

--- php/signup.php
+++ php/signup.php
@@ -1,32 +1,21 @@
 <?php ob_start(); ?>
 <?php
-$server="localhost";
-$username="root";
-$password="";
-$database="billing";
-$conn = new mysqli($server, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("connection failed:" . $conn->connect_error);
-}
-
 
  //This gets all the other information from the form
  $Email=$_POST['Email'] ;
  $Password= $_POST['Password'] ;
  $PasswordConfirm= $_POST['PasswordConfirm'] ;
- $Hashed= password_hash('$Password', PASSWORD_DEFAULT, ['cost' => 10]);
- //$Salt= $_POST['Salt'] ;
+ $HashedPassword= password_hash('$Password', PASSWORD_DEFAULT, ['cost' => 10]);
  $Mobile= $_POST['Mobile'] ;
  $Updated_by= $_POST['Updated_by'] ;
 
  // Connects to your Database
  //$conn = new mysqli("localhost", "root", "", "billing") ;
  //mysql_select_db("billing") or die(mysql_error()) ;
-
+$con = mysqli_connect('localhost', 'root', '', 'billing');
  //Writes the information to the database
- $sql="INSERT INTO `login`(Email,Password,PasswordConfirm,Hashed,Mobile,Updated_date,Updated_by)
-		 VALUES ('$Email','$Password','$PasswordConfirm','$Hashed','$Mobile',now(),'$Updated_by')";
+ $sql="INSERT INTO login (Email,Password,PasswordConfirm,HashedPassword,Mobile,Updated_date,Updated_by)
+		 VALUES ('$Email','$Password','$PasswordConfirm','$HashedPassword','$Mobile',now(),'$Updated_by')";
 
  header("Location: ../main.php");
  ?>
