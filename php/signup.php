e554c6e28da87002976260e7b993278eefd3a090
Sanorous <30567881+Sanorous@users.noreply.github.com>
2017-08-10 22:05:52 +0530

    change

--- php/signup.php
+++ php/signup.php
@@ -13,12 +13,10 @@
  $HashedPassword= password_hash('$Password', PASSWORD_DEFAULT, ['cost' => 10]);
 
  // Connects to your Database
- //$conn = new mysqli("localhost", "root", "", "billing") ;
- //mysql_select_db("billing") or die(mysql_error()) ;
-$con = mysqli_connect('localhost', 'root', '', 'billing');
+ $con = mysqli_connect('localhost', 'root', '', 'billing');
  //Writes the information to the database
- $sql="INSERT INTO login (Email,Password,PasswordConfirm,HashedPassword,Mobile,Updated_date,Updated_by)
-		 VALUES ('$Email','$Password','$PasswordConfirm','$HashedPassword','$Mobile',now(),'$Updated_by')";
+ $sql="INSERT INTO login (Email,Password,HashedPassword,Mobile,Updated_date,Updated_by)
+		 VALUES ('$Email','$Password','$HashedPassword','$Mobile',now(),'$Updated_by')";
 
  //header("Location: ../main.php");
 echo "true";
