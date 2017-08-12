b17a191baeb69fb100a3cadfb6625ee085d82cf6
Sanorous <30567881+Sanorous@users.noreply.github.com>
2017-08-12 22:12:24 +0530

    selecting only password from database
    rest of the things working but individually but togetther they are not working

--- php/login.php
+++ php/login.php
@@ -7,7 +7,7 @@ $data = json_decode(file_get_contents("php://input"));
 $Email=$data->email;
 $Password=$data->password;
 
-$result =	mysqli_query($conn,"SELECT * FROM login WHERE Email='$Email' ");
+$result =	mysqli_query($conn,"SELECT Password FROM login WHERE Email='$Email' ");
 
 				while($row = mysqli_fetch_array($result))
 				  {
@@ -17,7 +17,6 @@ $result =	mysqli_query($conn,"SELECT * FROM login WHERE Email='$Email' ");
 					//http://php.net/manual/en/function.password-verify.php
 				if (password_verify($Password, $PasswordFromDB))
 				{
-
           echo "true";
 				}
 				else
