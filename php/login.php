0c428a7abb8c1654658bb736734234223dc56f52
Sanorous <30567881+Sanorous@users.noreply.github.com>
2017-08-13 14:16:34 +0530

    trying

--- php/login.php
+++ php/login.php
@@ -7,6 +7,7 @@ $data = json_decode(file_get_contents("php://input"));
 $Email=$data->email;
 $Password=$data->password;
 
+
 $result =	mysqli_query($conn,"SELECT Password FROM login WHERE Email='$Email' ");
 
 				while($row = mysqli_fetch_array($result))
